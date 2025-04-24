caa60b8fe131bb49615af05d27bbb2b3dcd2d3c7
Oxi Ego <oxiego0@gmail.com>
2025-04-24 09:59:10 +0200

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ItemComponent } from "./item/item.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ItemComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true
 })
 export class AppComponent {
   title = 'GetOutFE';
